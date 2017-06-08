e084048701cd6e7474a26ab90ae9d4c125052ec5
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-08 15:09:34 -0700

    added email confirmation feature

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,10 @@
+class Notifier < ApplicationMailer
+     default from: 'no-reply@jungle.com',
+          return_path: 'no-reply@jungle.com'
+
+  def confirmation(order)
+    @order = order
+    mail(to: @order.email,
+    bcc: ["itsalysia@gmail.com"])
+  end
+end
