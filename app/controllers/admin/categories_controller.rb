2e306fb2d92577201832ca9955e6ab5f2cc05482
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-08 15:52:09 -0700

    implement HTTP basic auth

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
-
+  http_basic_authenticate_with :name => ENV['ADMIN_USER_NAME'], :password => ENV['ADMIN_PASSWORD']
+  
   def index
     @category  = Category.order(id: :desc).all
   end
